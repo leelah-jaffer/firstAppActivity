92b345818d56f82ebb7a70114500c78f5e67a200
leelah-jaffer <jafferleelah6@gmail.com>
2026-07-22 23:17:43 +0200

    changed color of backgroud

--- firstcoolapp/App.tsx
+++ firstcoolapp/App.tsx
@@ -20,7 +20,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f1168b',
+    backgroundColor: '#cf056a',
     alignItems: 'center',
     justifyContent: 'center',
   },
