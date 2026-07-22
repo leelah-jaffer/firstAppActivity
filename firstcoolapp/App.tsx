18645a58f6e1a83bb829a33bafb0557d57173976
leelah-jaffer <jafferleelah6@gmail.com>
2026-07-22 22:10:35 +0200

    made a new app, then creates a placeholder for the name and surname of the user as well as a button

---
+++ firstcoolapp/App.tsx
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to my App!</Text>
+      <Text>enter your name:</Text>
+      <TextInput placeholder="Jane"/>
+      <Text>Enter in your name</Text>
+      <TextInput placeholder="Angie"/>
+
+      <Button title="Add user"/>"
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f1168b',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
